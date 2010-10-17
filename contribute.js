e242a838cd6611ef4a61fc6ecab955459432d40f
leon <leon@nooch.lan>
2010-10-16 21:22:33 -0400

    fix

--- contribute.js
+++ contribute.js
@@ -1,1 +1,1 @@
-$('#widgit-action-wrapper ul').append('<li><a href="http://www.actblue.com/entity/fundraisers/24729">Contribute</a></li>');
+$document.ready(function(){$('#widgit-action-wrapper ul').append('<li><a href="http://www.actblue.com/entity/fundraisers/24729">Contribute</a></li>');});
